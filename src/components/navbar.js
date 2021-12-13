782f23a1be8293d9d1db617532bfe1923b069e8e
Atul Nirbhavane <anirbhavane106@gmail.com>
2021-12-13 20:01:12 +0530

    Initial Commit

---
+++ src/components/navbar.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const navbar = () => {
+    return (
+        <div>
+            <ul className='navbarList'>
+                <li><a>Home</a></li>
+                <li>Projects</li>
+                <li>Blogs</li>
+                <li>Magic</li>
+            </ul>
+        </div>
+    )
+}
+
+export default navbar
