f0e9e454d4885840393f443d47b94b2d25530084
Atul Nirbhavane <anirbhavane106@gmail.com>
2021-12-18 05:58:26 +0530

    Profile-links added

--- src/App.js
+++ src/App.js
@@ -5,8 +5,18 @@ function App() {
   return (
     <div className="App">
       <Navbar />
-      <h1>Hi I'm Atul Nirbhavane</h1>
-      <h4>a Web Developer</h4>
+      <div className="introduction">
+        <h1>Hi I'm Atul Nirbhavane</h1>
+        <h4>a Web Developer</h4>
+      </div>
+
+      <div className="profile-links-div">
+        <ul>
+          <li><a href="https://twitter.com/underager_d" target="_blank" rel="noreferrer">Twitter</a></li>
+          <li><a href="https://github.com/underager"target="_blank" rel="noreferrer">Github</a></li>
+          <li><a href="https://www.linkedin.com/in/atul-nirbhavane-554067b4/" target="_blank" rel="noreferrer">LinkedIn</a></li>
+        </ul>
+      </div>
     </div>
   );
 }
